69541b2584a04ce7b443cc44f87f6fe1f802c7e5
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-03-18 10:08:18 -0300

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso_de_java;
+
+import java.util.Scanner;
+/**
+ *
+ * @author pc06
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner (System.in);
+        
+        String nombre;
+        double a;
+        double b;
+        double c;
+        double d;
+        
+         System.out.println("Ingrese nombre del trabajador: ");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Ingrese costo por hora de trabajo: ");
+        a = entrada.nextDouble();
+        
+        System.out.println("Ingrese hs que va a trabajar" +nombre+ "en el mes: ");
+        b = entrada.nextDouble();
+        
+        c = a*b;
+        d = c*12;
+        
+        System.out.println("El salario de "+nombre+  " será de: " + c);
+        System.out.println("El ingreso anual de " +nombre+ " será de: " + d);
+        
+    }
+    
+}
