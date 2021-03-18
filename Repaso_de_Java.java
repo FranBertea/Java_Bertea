fe366219b1e89a41358b76a8e4b69bc7e85006a9
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-03-18 08:56:28 -0300

    Add files via upload

---
+++ Repaso_de_Java.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso_de_java;
+
+/**
+ *
+ * @author pc06
+ */
+public class Repaso_de_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=4;
+        int b=7;
+        int res;
+        res= a+b;
+        System.out.println("El resultado es: "+res);
+    }
+    
+}
