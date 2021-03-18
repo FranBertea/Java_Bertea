31b8b15395507541e8be8c577c6c1995249704a5
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-03-18 09:40:04 -0300

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso_de_java;
+
+/**
+ *
+ * @author pc06
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int a=10;
+       int b=5;
+       int suma=a+b;
+       int resta=a-b;
+       int multiplicar=a*b;
+       int division=a/b;
+       
+       System.out.println("La suma de "+a+" más "+b+" es igual a: "+suma );
+       System.out.println("La resta de "+a+" menos "+b+" es igual a: "+resta );
+    }
+    
+}
